01bb158dc9cdfeec526aa569756cab9afbfa2000
Anastasia <n41111215@mail.ru>
2020-12-16 16:56:03 +0300

    add CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,18 +1,18 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldCalculateIfAmountCorrect() {
-        int amount = 450;
+        int amount = 5700;
         int actual = service.remain(amount);
-        int expected = 550;
-        assertEquals(actual, expected);
+        int expected = 300;
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -20,17 +20,22 @@ public class CashbackHackServiceTest {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldCalculateIfAmountOver1000() {
-        int amount = 1005;
-
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 995;
-
-        assertEquals(actual, expected);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldCalculateIfAmountIncorrect() {
+        int amount = -50000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
